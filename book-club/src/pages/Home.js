ff49f741ed3088b4250ceb40b0662f7db63a6726
Mohamed Aharrat <mohamedaharrat1@gmail.com>
2021-09-25 17:28:32 +0200

    App Baseline

---
+++ book-club/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Grid from '@material-ui/core/Grid';
+
+const Home = () => {
+    return (
+      <Grid contaienr>
+        <div>
+          <h1>Welcome to Book Club!</h1>
+          <p>Get started by adding your friends!</p>
+        </div>
+      </Grid>
+    );
+}
+
+export default Home;  
